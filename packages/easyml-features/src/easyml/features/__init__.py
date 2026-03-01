05a4d98972991f5e8210f492ecd90bb11c060a52
Michael S <msilverblatt@gmail.com>
2026-02-28 21:04:18 -0500

    feat(features): finalize package exports

--- packages/easyml-features/src/easyml/features/__init__.py
+++ packages/easyml-features/src/easyml/features/__init__.py
@@ -1,1 +1,7 @@
 """Feature registry and engineering for EasyML."""
+from easyml.features.registry import FeatureRegistry
+from easyml.features.resolver import FeatureResolver
+from easyml.features.builder import FeatureBuilder
+from easyml.features.pairwise import PairwiseFeatureBuilder
+
+__all__ = ["FeatureRegistry", "FeatureResolver", "FeatureBuilder", "PairwiseFeatureBuilder"]
