d0ae2c72b4af5d430100c34d4e4657845ba1b892
Michael S <msilverblatt@gmail.com>
2026-03-06 17:10:44 -0500

    feat(competitions): add Pydantic v2 schemas for competition engine

---
+++ packages/easyml-sports/src/easyml/sports/competitions/__init__.py
@@ -0,0 +1,31 @@
+"""Competition engine schemas and data models for bracket/tournament simulation."""
+
+from easyml.sports.competitions.schemas import (
+    AdjustmentConfig,
+    CompetitionConfig,
+    CompetitionFormat,
+    CompetitionResult,
+    CompetitionStructure,
+    GroupConfig,
+    KnockoutConfig,
+    MatchupContext,
+    ScoreResult,
+    ScoringConfig,
+    SeedingMode,
+    StandingsEntry,
+)
+
+__all__ = [
+    "AdjustmentConfig",
+    "CompetitionConfig",
+    "CompetitionFormat",
+    "CompetitionResult",
+    "CompetitionStructure",
+    "GroupConfig",
+    "KnockoutConfig",
+    "MatchupContext",
+    "ScoreResult",
+    "ScoringConfig",
+    "SeedingMode",
+    "StandingsEntry",
+]
