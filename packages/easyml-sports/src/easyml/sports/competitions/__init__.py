34d10ec2d702f2d3cae332197d018ca953b89574
Michael S <msilverblatt@gmail.com>
2026-03-06 17:29:06 -0500

    feat(competitions): add pool-aware bracket optimizer with pluggable strategies

--- packages/easyml-sports/src/easyml/sports/competitions/__init__.py
+++ packages/easyml-sports/src/easyml/sports/competitions/__init__.py
@@ -15,14 +15,21 @@ from easyml.sports.competitions.schemas import (
     StandingsEntry,
 )
 from easyml.sports.competitions.adjustments import apply_adjustments
+from easyml.sports.competitions.optimizer import (
+    BUILTIN_STRATEGIES,
+    CompetitionOptimizer,
+    StrategyFn,
+)
 from easyml.sports.competitions.scorer import CompetitionScorer
 from easyml.sports.competitions.simulator import CompetitionSimulator
 from easyml.sports.competitions.structure import build_structure
 
 __all__ = [
     "AdjustmentConfig",
+    "BUILTIN_STRATEGIES",
     "CompetitionConfig",
     "CompetitionFormat",
+    "CompetitionOptimizer",
     "CompetitionResult",
     "CompetitionScorer",
     "CompetitionSimulator",
@@ -34,6 +41,7 @@ __all__ = [
     "ScoringConfig",
     "SeedingMode",
     "StandingsEntry",
+    "StrategyFn",
     "apply_adjustments",
     "build_structure",
 ]
